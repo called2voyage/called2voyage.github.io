6b846d145a5f26edc2aa191046d58a44c909e100
called2voyage <thesboys@gmail.com>
2025-08-22 16:31:11 -0600

    Update site to use Shiver Dock personal site template

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.get('/', function(req, res) {
 
 // end of pages
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+app.listen(9100);
+console.log('Server is listening on port 9100');
